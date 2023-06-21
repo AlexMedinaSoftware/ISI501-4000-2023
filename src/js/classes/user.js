3aed11b077d16d5d5dc6bb0e35b62ca623b7db8e
AlexMedina <alexcarcamomedina@hotmail.com>
2023-06-20 20:41:50 -0400

    cache service worker

---
+++ src/js/classes/user.js
@@ -0,0 +1,12 @@
+class User{
+
+    name = "John Doe";
+    uuid = "000000000000";
+    volumes = {
+        menu: 0.1,
+        game: 0.2,
+        sounds: 1,
+    }
+    volumesMuted = false;
+    
+}
---
+++ src/js/modules/sw.js
@@ -0,0 +1,32 @@
+// Establish a cache name
+const cacheName = 'ChileanMadMax_v1';
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(caches.open(cacheName));
+});
+
+self.addEventListener('fetch', (event) => {
+    // Check if this is a request for an image
+    if (event.request.destination === 'image' || event.request.destination === 'audio' || event.request.destination === 'video') {
+        event.respondWith(caches.open(cacheName).then((cache) => {
+            // Go to the cache first
+            return cache.match(event.request.url).then((cachedResponse) => {
+                // Return a cached response if we have one
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+
+                // Otherwise, hit the network
+                return fetch(event.request).then((fetchedResponse) => {
+                    // Add the network response to the cache for later visits
+                    cache.put(event.request, fetchedResponse.clone());
+
+                    // Return the network response
+                    return fetchedResponse;
+                });
+            });
+        }));
+    } else {
+        return;
+    }
+});
